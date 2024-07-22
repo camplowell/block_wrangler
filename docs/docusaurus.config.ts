8e6586b167f3d6017b8ba20065f390afc8b26592
camplowell <camplowell@gmail.com>
2024-07-22 11:03:35 -0400

    Rename tutorial to documentation

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -63,7 +63,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Documentation',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
