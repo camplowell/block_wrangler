5e820968046bde2e199babc85a1d0ab7a19943d3
camplowell <camplowell@gmail.com>
2025-05-03 02:11:11 -0400

    Update plants

--- block_wrangler/library/tags/plants.py
+++ block_wrangler/library/tags/plants.py
@@ -10,7 +10,6 @@ def load_tags(library:TagLibrary):
 	library.touch('plant/saplings').add(library.touch('minecraft:saplings'))
 
 	library.touch('plant/thin') \
-		.add(library.touch('minecraft:flowers')) \
 		.add(library.touch('minecraft:crops')) \
 		.add(library.touch('minecraft:flower_pots')) \
 		.add(library.touch('minecraft:saplings')) \
@@ -19,24 +18,36 @@ def load_tags(library:TagLibrary):
 			'minecraft:bamboo_sapling',
 			'minecraft:big_dripleaf',
 			'minecraft:big_dripleaf_stem',
+			'minecraft:bush',
+			'minecraft:cactus_flower',
 			'minecraft:cave_vines',
 			'minecraft:dead_bush',
 			'minecraft:fern',
+			'minecraft:firefly_bush',
 			'minecraft:hanging_roots',
+			'minecraft:kelp',
+			'minecraft:kelp_plant',
 			'minecraft:large_fern',
+			'minecraft:lilac',
 			'minecraft:lily_pad',
 			'minecraft:mangrove_propagule',
 			'minecraft:mangrove_roots',
+			'minecraft:peony',
 			'minecraft:pink_petals',
 			'minecraft:pitcher_plant',
+			'minecraft:rose_bush',
 			'minecraft:seagrass',
+			'minecraft:short_dry_grass',
 			'minecraft:short_grass',
 			'minecraft:small_dripleaf',
 			'minecraft:spore_blossom',
 			'minecraft:sugar_cane',
+			'minecraft:sunflower',
 			'minecraft:sweet_berry_bush',
+			'minecraft:tall_dry_grass',
 			'minecraft:tall_grass',
 			'minecraft:vine',
+			'minecraft:wildflowers'
 		))
 	
 	library.touch('plant/thick') \
@@ -62,4 +73,23 @@ def load_tags(library:TagLibrary):
 			'minecraft:melon_stem',
 			'minecraft:pumpkin_stem',
 			'minecraft:mangrove_roots',
+		))
+
+	library.touch('plant/moss') \
+		.add(block_types(
+			'minecraft:moss_block',
+			'minecraft:moss_carpet',
+		))
+	
+	library.touch('plant/pale_moss') \
+		.add(block_types(
+			'minecraft:pale_moss_block',
+			'minecraft:pale_moss_carpet',
+		))
+
+	library.touch('plant/groundcover') \
+		.add(block_types(
+			'minecraft:leaf_litter',
+			'minecraft:pink_petals',
+			'minecraft:wildflowers'
 		))
